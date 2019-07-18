a1e057bdd083fa955fb1f92b638b720c6acf8487
JustinNeilY <CoderJNYao@outlook.com>
2019-07-18 21:29:49 +0800

    static file

---
+++ logistics-manager-web/src/main/java/com/yjn/controller/PageController.java
@@ -0,0 +1,34 @@
+package com.yjn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面控制器
+ * @author JustinNeil
+ *
+ */
+@Controller
+public class PageController {
+
+    /**
+     * 跟目录就跳转到main.jsp页面
+     * @return
+     */
+    @RequestMapping("/")
+    public String showMain(){
+        return "main";
+    }
+    
+    /**
+     * restful 风格
+     *    传递的是什么数据就跳转到对应的页面
+     * @param path
+     * @return
+     */
+    @RequestMapping("/{path}")
+    public String showTop(@PathVariable String path){
+        return path;
+    }
+}
